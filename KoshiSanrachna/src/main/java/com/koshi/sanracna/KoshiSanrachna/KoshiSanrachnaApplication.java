333465c63b51b703c7c12680744a57ed24cb05b5
kundan thakur <kundan@zwayam.com>
2020-06-10 23:38:22 +0530

    first commit

---
+++ KoshiSanrachna/src/main/java/com/koshi/sanracna/KoshiSanrachna/KoshiSanrachnaApplication.java
@@ -0,0 +1,13 @@
+package com.koshi.sanracna.KoshiSanrachna;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KoshiSanrachnaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KoshiSanrachnaApplication.class, args);
+	}
+
+}
---
+++ KoshiSanrachna/src/test/java/com/koshi/sanracna/KoshiSanrachna/KoshiSanrachnaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.koshi.sanracna.KoshiSanrachna;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KoshiSanrachnaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
